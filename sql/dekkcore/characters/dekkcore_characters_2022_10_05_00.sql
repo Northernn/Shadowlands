d8bd75150d5989944ca19e5c6b13b16fbe265dab
Northernn <nick@doughnuts.co.uk>
2026-01-30 09:51:06 +0000

    ignore sql

--- sql/dekkcore/characters/dekkcore_characters_2022_10_05_00.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS `character_instance`;
-DROP TABLE IF EXISTS `group_instance`;
-DROP TABLE IF EXISTS `instance_reset`;
-DROP TABLE IF EXISTS `instance_scenario_progress`;
--- sql/dekkcore/world/dekkcore_world_2022_09_07_01.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `spell_script_names` WHERE `spell_id`=217200;
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (217200, 'spell_hun_barbed_shot');
